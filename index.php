56ab96ff212bd7843570681d347b32ed205a086a
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-09-02 17:06:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,26 +22,30 @@
 				</div>
 			<div>
 			<div id="content">
-                <img src="images/header.jpg" width="100%">
-                <div id="char_categories">
-                    <h1>Choose a Side</h1>
-                    <div class="swingimageright">
-                        <img src="images/hunter.jpg" width="80%">
-                        <a href=""><h2>HUNTER</h2></a>
-                    </div>
-                    <div class="swingimageleft">
-                        <img src="images/survivor.jpg" width="80%">
-                        <a href=""><h2>SURVIVOR</h2></a>
-                    </div>
-                    <img src="images/tacks.png" width="6%" style="position: relative; left: -240px; bottom: -20px;">
-                    <img src="images/tacks.png" width="6%" style="position: relative; left: 340px; bottom: -20px;">
-                </div>
+				<img src="images/header.jpg" width="100%">
+				<a href="https://h55na.gdl.netease.com/identityv_setup_release_oversea_0828.exe">
+					<div id="download">
+						<h1>Download Now!</h1>
+					</div>
+				</a>
+               			<div id="char_categories">
+					<h1>Choose a Side</h1>
+					<hr style="width: 35%; border-color: red; left: 4px;">
+                    			<div class="swingimageright">
+                        			<img src="images/hunter.jpg" width="80%">
+                        			<a href=""><h2>HUNTER</h2></a>
+                    			</div>
+                    			<div class="swingimageleft">
+                        			<img src="images/survivor.jpg" width="80%">
+                        			<a href=""><h2>SURVIVOR</h2></a>
+                    			</div>
+                    			<img src="images/tacks.png" width="6%" style="position: relative; left: -240px; bottom: -20px;">
+                    			<img src="images/tacks.png" width="6%" style="position: relative; left: 340px; bottom: -20px;">
+                		</div>
+			</div>
+			<div id="footer">
+		     		<h3 style="text-align: center"> Github: <a href="https://github.com/Yeshuya/CSS"> https://github.com/Yeshuya/CSS </a></h1>
 			</div>
 		</div>
-		<div id="footer">
-		     <h3 style="text-align: center"> Github: <a href="https://github.com/Yeshuya/CSS"> https://github.com/Yeshuya/CSS </a></h1>
-		</div>
 	</body>
-
-
 </html>
